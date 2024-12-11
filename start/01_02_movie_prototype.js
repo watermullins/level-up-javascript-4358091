5ceb026a41dc68e2aebc8f43d9b3215f7abdfdff
watermullins <stuartmullins42@gmail.com>
2024-12-11 13:07:03 +0000

    movie prototype

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,19 @@
-// Write your code here
+class Movie{
+
+  constructor(title, director, genre, year, rating){
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.year = year;
+    this.rating = rating;
+  }
+
+  getOverview(){
+    return(this.title + ", a " + this.genre + " film directed by " + this.director + 
+      " was relased in " + this.year + ". It received a rating of " + this.rating + '.');
+  }
+}
+
+m1 = new Movie("The Hobbit", "Peter Jackson", "Fantasy", 2012, 5);
+overview = m1.getOverview();
+console.log(overview);
